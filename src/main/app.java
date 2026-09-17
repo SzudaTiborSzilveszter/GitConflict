0b159477b4f96cb1aad89da014451387431bb4d6
molnarbea <molnarbeatrix11@gmail.com>
2026-09-17 15:48:21 +0200

    hihi

--- src/main/app.java
+++ src/main/app.java
@@ -5,6 +5,7 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Bea");
         System.out.println("Tiboriusz");
+        System.out.println("hehe");
     }
     
 }
